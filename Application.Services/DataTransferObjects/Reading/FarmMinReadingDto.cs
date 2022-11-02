dd4493ec0b5165d05849772882dcc182f6c7c1ca
Stas <markovszzzz@gmail.com>
2022-11-02 17:23:25 +0300

    farm reading dtos were updated

--- Application.Services/DataTransferObjects/Reading/FarmMinReadingDto.cs
+++ Application.Services/DataTransferObjects/Reading/FarmMinReadingDto.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Application.Services.DataTransferObjects.Reading
 {
-    public class FarmReadingDto
+    public class FarmMinReadingDto
     {
         public string Name { get; set; }
         public int DeadPetsCount { get; set; }
@@ -12,8 +11,5 @@ namespace Application.Services.DataTransferObjects.Reading
         public TimeSpan AverageThirstQuenchingTime { get; set; }
         public int AverageHappinessDaysCount { get; set; }
         public int AveragePetsAge { get; set; }
-
-        public IEnumerable<PetReadingDto> Pets { get; set; }
-        public IEnumerable<FarmFriendReadingDto> FarmFriends { get; set; }
     }
 }
