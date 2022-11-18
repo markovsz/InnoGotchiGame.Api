d8fde6724fedde2880895d6eab8efe0fadab5f03
Stas <markovszzzz@gmail.com>
2022-11-18 20:26:47 +0300

    auth controller was added

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services.DataTransferObjects;
+using Application.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("signIn")]
+        public async Task<IActionResult> SignInAsync(UserAuthenticationDto authDto)
+        {
+            var accessDto = await _authService.SignInAsync(authDto);
+            return Ok(accessDto);
+        }
+    }
+}
