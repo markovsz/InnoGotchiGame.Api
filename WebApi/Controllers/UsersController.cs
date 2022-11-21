fa139ba223c4ee40e00e1e6f033cdf069ed75872
Stas <markovszzzz@gmail.com>
2022-11-21 03:17:39 +0300

    users controller was added

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Application.Services.DataTransferObjects.Creating;
+using Application.Services.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Filters;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUsersService _usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUserAsync([FromBody] UserCreatingDto userDto)
+        {
+            var userId = await _usersService.CreateUserAsync(userDto);
+            return Created($"{userId}", new { Id = userId});
+        }
+
+        [Authorize]
+        [HttpGet("{userId}", Name = "GetUserInfo")]
+        public async Task<IActionResult> GetUserInfoByIdAsync(Guid userId)
+        {
+            var userInfo = await _usersService.GetUserInfoByIdAsync(userId);
+            return Ok(userInfo);
+        }
+
+        [Authorize]
+        [ServiceFilter(typeof(ExtractUserIdFilter))]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUserByIdAsync(Guid userId)
+        {
+            await _usersService.DeleteUserByIdAsync(userId);
+            return NoContent();
+        }
+    }
+}
