2c63d5be1be46aa866b1a24f4f7c6d21324a07c4
Stas <markovszzzz@gmail.com>
2022-12-12 13:56:02 +0300

    pet reading dto was updated (body part's pictures, positions and scales)

--- Application.Services/DataTransferObjects/Reading/PetReadingDto.cs
+++ Application.Services/DataTransferObjects/Reading/PetReadingDto.cs
@@ -11,5 +11,22 @@ namespace Application.Services.DataTransferObjects.Reading
         public string ThirstLevel { get; set; }
         public int HappinessDaysCount { get; set; }
         public string IsAlive { get; set; }
+
+        public string BodyPicName { get; set; }
+        public int BodyPictureX { get; set; }
+        public int BodyPictureY { get; set; }
+        public float BodyPictureScale { get; set; }
+        public string EyesPicName { get; set; }
+        public int EyesPictureX { get; set; }
+        public int EyesPictureY { get; set; }
+        public float EyesPictureScale { get; set; }
+        public string MouthPicName { get; set; }
+        public int MouthPictureX { get; set; }
+        public int MouthPictureY { get; set; }
+        public float MouthPictureScale { get; set; }
+        public string NosePicName { get; set; }
+        public int NosePictureX { get; set; }
+        public int NosePictureY { get; set; }
+        public float NosePictureScale { get; set; }
     }
 }
