09fe1a9087609538bce19e33f28e2a458280a92b
Stas <markovszzzz@gmail.com>
2022-12-12 13:14:58 +0300

    body part's picture properties were added

--- Application.Services/DataTransferObjects/Creating/PetCreatingDto.cs
+++ Application.Services/DataTransferObjects/Creating/PetCreatingDto.cs
@@ -6,5 +6,10 @@ namespace Application.Services.DataTransferObjects.Creating
     {
         public string Name { get; set; }
         public Guid FarmId { get; set; }
+
+        public string BodyPicName { get; set; }
+        public string EyesPicName { get; set; }
+        public string MouthPicName { get; set; }
+        public string NosePicName { get; set; }
     }
 }
