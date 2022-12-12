947f3b7324f4ef7c6230617b4a303ef46b797a70
Stas <markovszzzz@gmail.com>
2022-12-12 13:56:23 +0300

    pet creating dto was updated (body part's positions and scales)

--- Application.Services/DataTransferObjects/Creating/PetCreatingDto.cs
+++ Application.Services/DataTransferObjects/Creating/PetCreatingDto.cs
@@ -8,8 +8,20 @@ namespace Application.Services.DataTransferObjects.Creating
         public Guid FarmId { get; set; }
 
         public string BodyPicName { get; set; }
+        public int BodyPictureX { get; set; }
+        public int BodyPictureY { get; set; }
+        public float BodyPictureScale { get; set; }
         public string EyesPicName { get; set; }
+        public int EyesPictureX { get; set; }
+        public int EyesPictureY { get; set; }
+        public float EyesPictureScale { get; set; }
         public string MouthPicName { get; set; }
+        public int MouthPictureX { get; set; }
+        public int MouthPictureY { get; set; }
+        public float MouthPictureScale { get; set; }
         public string NosePicName { get; set; }
+        public int NosePictureX { get; set; }
+        public int NosePictureY { get; set; }
+        public float NosePictureScale { get; set; }
     }
 }
