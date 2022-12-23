1ac8bc2959f6e89561a11b829553bed3dae40471
Stas <markovszzzz@gmail.com>
2022-12-23 20:30:03 +0300

    useless endpoint was deleted + deletion endpoint was renamed

--- WebApi/Controllers/FarmFriendsController.cs
+++ WebApi/Controllers/FarmFriendsController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Filters;
 
 namespace WebApi.Controllers
 {
@@ -22,26 +23,20 @@ namespace WebApi.Controllers
         }
 
         [Authorize]
+        [ServiceFilter(typeof(ExtractUserIdFilter))]
         [HttpPost]
-        public async Task<IActionResult> CreateFarmFriendAsync([FromBody] FarmFriendCreatingDto farmDto)
+        public async Task<IActionResult> CreateFarmFriendAsync([FromBody] FarmFriendCreatingDto farmDto, Guid userId)
         {
-            var farmFriendId = await _farmFriendsService.CreateFarmFriendAsync(farmDto);
+            var farmFriendId = await _farmFriendsService.CreateFarmFriendAsync(farmDto, userId);
             return Created($"{farmFriendId}", new { Id = farmFriendId });
         }
 
         [Authorize]
-        [HttpGet("user/{userId}")]
-        public async Task<IActionResult> GetUserFarmFriendsAsync(Guid userId)
+        [ServiceFilter(typeof(ExtractUserIdFilter))]
+        [HttpDelete("farm-friend/{farmFriendId}")]
+        public async Task<IActionResult> DeleteFarmFriendByIdAsync(Guid farmFriendId, Guid userId)
         {
-            var farmFriends = await _farmFriendsService.GetUserFarmFriendsAsync(userId);
-            return Ok(farmFriends);
-        }
-
-        [Authorize]
-        [HttpDelete("user/{userId}")]
-        public async Task<IActionResult> DeleteFarmFriendByIdAsync(Guid userId)
-        {
-            await _farmFriendsService.DeleteFarmFriendByIdAsync(userId);
+            await _farmFriendsService.DeleteFarmFriendByIdAsync(farmFriendId, userId);
             return NoContent();
         }
     }
